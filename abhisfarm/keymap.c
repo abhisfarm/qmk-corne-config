9fe8f02c591c27af18dd12aaf5b0f852af316133
abhisfarm <manikandanmanivelu@gmail.com>
2023-09-06 18:45:21 +0800

    updated keymap

--- abhisfarm/keymap.c
+++ abhisfarm/keymap.c
@@ -30,6 +30,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KC_FSC LCTL(LALT(KC_F))
 #define KC_BEYOND LCTL(LALT(KC_B))
 
+//Mod Taps
+#define CTL_CAPS MT(MOD_LCTL, KC_CAPS)
+
+//Layer Taps
+#define LOW_ENT LT(KC_LOWER, KC_ENT)
+#define LOW_BSPC LT(KC_LOWER, KC_BSPC)
+#define RAI_SPC LT(KC_RAISE, KC_SPC)
+#define RAI_DEL LT(KC_RAISE, KC_DEL)
+
 enum sofle_layers {
     _DEFAULTS = 0,
     _QWERTY = 0,
@@ -50,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+     CTL_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT,KC_LOWER,  KC_ENT,     KC_SPC,KC_RAISE,  KC_RGUI
+                                          KC_LALT, RAI_DEL, LOW_ENT,    RAI_SPC,LOW_BSPC,  KC_RGUI
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -63,23 +72,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX,KC_BEYOND, KC_FSC, KC_SNIP,                      KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,  KC_F11, KC_F12,
+     CTL_CAPS, XXXXXXX, XXXXXXX,KC_BEYOND, KC_FSC, KC_SNIP,                      KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT,  KC_F11, KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,KC_SDISP,KC_UNLOCK,                      KC_HOME, KC_END, KC_CHOME, KC_CEND, KC_PGUP, KC_PGDN,
+      KC_LSFT, KC_WCMD, KC_SMSC,KC_TMGR,KC_SDISP,KC_UNLOCK,                      KC_HOME, KC_END, KC_CHOME, KC_CEND, KC_PGUP, KC_PGDN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, _______,  KC_ENT,     KC_SPC, _______, KC_RGUI
+                                          KC_LALT, RAI_DEL, LOW_ENT,    RAI_SPC,LOW_BSPC, KC_RGUI
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+     CTL_CAPS,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_ENT,     KC_SPC, _______, KC_RGUI
+                                          KC_LGUI, RAI_DEL, LOW_ENT,    RAI_SPC,LOW_BSPC, KC_RGUI
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -91,13 +100,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, _______,  KC_ENT,     KC_SPC, _______, KC_RGUI
+                                          KC_LALT, RAI_DEL, LOW_ENT,    RAI_SPC,LOW_BSPC, KC_RGUI
                                       //`--------------------------'  `--------------------------'
   )
 };
 
 #ifdef RGBLIGHT_ENABLE
 void keyboard_post_init_user(void) {
+	
 	rgblight_sethsv_noeeprom(HSV_TEAL);
 }
 #endif
